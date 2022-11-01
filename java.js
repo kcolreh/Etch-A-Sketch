71ff712a6409ff97f7847115c0f4be78bb7bebb0
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-01 21:04:54 +0100

    Create 16x16 grid of empty black squeres

--- java.js
+++ java.js
@@ -1,1 +1,7 @@
-console.log ('Test')
+const container = document.querySelector('.container');
+
+for( let i = 0; i < 256; i++){
+const grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
+}
