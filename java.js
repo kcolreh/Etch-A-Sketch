363b6203840921d8b32fe8dd419436e3c737984e
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-02 15:38:48 +0100

    Create 2 events: 1)Paints the divs made by the loop 2)Removes the paint from the divs

--- java.js
+++ java.js
@@ -1,7 +1,29 @@
 const container = document.querySelector('.container');
 
-for( let i = 0; i < 256; i++){
-const grid = document.createElement('div');
-grid.classList.add('grid');
-container.appendChild(grid);
+
+for (let i = 0; i < 256; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    container.appendChild(grid);
 }
+
+
+//Paints the divs when you hover your mouse over them
+const hover = document.querySelectorAll('.grid');
+hover.forEach((div) => {
+    div.addEventListener('mouseover', function () {
+        div.classList.add('change')
+    });
+});
+
+//Erses the paint when you hover your mouse over them
+const eraser = document.querySelector('#erase');
+eraser.addEventListener('click', function () {
+    const hover2 = document.querySelectorAll('.grid');
+    hover2.forEach((div) => {
+        div.addEventListener('mouseover', function () {
+            div.classList.remove('change')
+        });
+    });
+});
+
