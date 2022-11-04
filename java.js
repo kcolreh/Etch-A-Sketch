fe7ba546059501f6556d77fad86e693b021062d0
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-04 18:00:29 +0100

    Create color wheel for user to choose imput and rainbow mode for random colors

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ const slider = document.querySelector('#range');
 const grid = document.querySelectorAll('.grid');
 
 
-createGrid(); 
+createGrid();
 
 slider.addEventListener('change', function () {
     let boxes = document.querySelectorAll('.grid');
@@ -11,6 +11,8 @@ slider.addEventListener('change', function () {
         box.remove();//removes old grid
     });
     createGrid() //makes new grid afte reset
+
+
 })
 
 
@@ -24,35 +26,63 @@ function createGrid() {
         grid.style.height = boxSize + 'px';
         container.appendChild(grid);
     }
-}
-
-
-
-//Paints the divs when you hover your mouse over them;
-const hover = document.querySelectorAll('.grid');
-hover.forEach((div) => {
-    div.addEventListener('mouseover', function () {
-        div.classList.add('change');
+    //Paints the divs when you hover your mouse over them;
+    const hover = document.querySelectorAll('.grid');
+    hover.forEach((div) => {
+        div.addEventListener('mouseover', function () {
+            div.classList.add('change');
+        });
     });
-});
+
+}
 
 //Erases all the painted divs;
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', function () {
     const hover = document.querySelectorAll('.grid');
     hover.forEach((div) => {
-        div.classList.remove('change');
+        div.classList.remove('change', 'rainbow');
+        div.style.backgroundColor = '';
     })
 });
 
 //Erses the paint when you hover your mouse over them
 const eraser = document.querySelector('#erase');
 eraser.addEventListener('click', function () {
-    const hover2 = document.querySelectorAll('.grid');
-    hover2.forEach((div) => {
+    const boxes = document.querySelectorAll('.grid');
+    boxes.forEach((div) => {
         div.addEventListener('mouseover', function () {
-            div.classList.remove('change');
+            div.classList.remove('change', 'rainbow');
+            div.style.backgroundColor = '';
         });
     });
 });
+generateRandomColor ()
+const rainbow = document.querySelector('#rainbow');
+rainbow.addEventListener('click', function () {
+    const boxes = document.querySelectorAll('.grid');
+    boxes.forEach((div) => {
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = generateRandomColor ();
+        })
+    })
+})
 
+colorpicker.addEventListener('change', function () {
+    const boxes = document.querySelectorAll('.grid');
+    boxes.forEach((div) => {
+        div.addEventListener('mouseover', function () {
+            let color = colorpicker.value;
+            console.log(color)
+            div.style.backgroundColor = color;
+
+        })
+    })
+})
+
+function generateRandomColor (){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
